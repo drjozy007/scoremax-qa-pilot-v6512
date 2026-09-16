c547dfc1f74231c7e47515278fdb9cacfed17c1b
drjozy007 <drjozy007@yahoo.com>
2026-09-16 09:02:00 +0100

    Apply learner chapter open fix

--- deploy_ux_vnext.py
+++ deploy_ux_vnext.py
@@ -11,6 +11,7 @@ from ux_vnext_overlay.ux_programme_catalogue_routing import apply_programme_cata
 from ux_vnext_overlay.ux_main_subject_fsc_fence import apply_main_subject_fsc_fence
 from ux_vnext_overlay.ux_canonical_student_navigation import apply_canonical_student_navigation
 from ux_vnext_overlay.ux_restore_access_cards import apply_restore_access_cards
+from ux_vnext_overlay.ux_chapter_card_open_fix import apply_chapter_card_open_fix
 
 ROOT=Path('scoremax_runtime_v669b')
 
@@ -204,6 +205,7 @@ def main() -> None:
     apply_main_subject_fsc_fence(ROOT)
     apply_canonical_student_navigation(ROOT)
     apply_restore_access_cards(ROOT)
+    apply_chapter_card_open_fix(ROOT)
     _wire_programme_tabs_to_correct_surfaces()
 
 
